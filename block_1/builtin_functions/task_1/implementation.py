2c83c6dfbf156b3e4a76210597942108e4ee8e49
r.boldyrev <r.boldyrev@bars.group>
2024-06-12 01:48:23 +0700

    classes

--- block_1/builtin_functions/task_1/implementation.py
+++ block_1/builtin_functions/task_1/implementation.py
@@ -1,2 +1,42 @@
+from block_1.common import (
+    MyException,
+)
+
+
+def ex(function):
+    def wrap(arg1,arg2):
+        try:
+            return function(arg1,arg2)
+        except Exception:
+            raise MyException(Exception)
+
+    return wrap
+
+
 class Value:
-    pass
+    @ex
+    def __init__(self, value):
+        self.value_self = value
+
+    @ex
+    def __add__(self, other):
+        return self.value_self + other
+
+    @ex
+    def __sub__(self, other):
+        return self.value_self - other
+
+    @ex
+    def __mul__(self, other):
+        return self.value_self * other
+
+    @ex
+    def __truediv__(self, other):
+        return float(self.value_self) / other
+
+
+# ten = Value(10)
+# print(ten + 11)
+# print(ten * 8)
+# print(ten - 7)
+# print(ten / 2)
