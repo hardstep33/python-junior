4481410b705a1de9a317c7e90b2ee3f8b154f1b0
r.boldyrev <r.boldyrev@bars.group>
2024-06-12 01:13:37 +0700

    decorators

--- block_1/decorators/task_4/implementation.py
+++ block_1/decorators/task_4/implementation.py
@@ -1,4 +1,10 @@
-def decorator_maker():
+import time
+from block_1.common import (
+    MyException,
+)
+
+
+def decorator_maker(times, delay):
     """
     Обертка, которая повторяет вызов функции times раз с паузой delay секунд
     Args:
@@ -8,5 +14,18 @@ def decorator_maker():
     Returns:
         валидное значение (при вызове bool() -> True)
     """
-    raise NotImplementedError
 
+    def decorator_internal(function):
+        def wrap():
+            for i in range(1, times + 1):
+                try:
+                    return function()
+                except Exception:
+                    if i < times:
+                        time.sleep(delay)
+                    else:
+                        raise MyException(Exception)
+
+        return wrap
+
+    return decorator_internal
