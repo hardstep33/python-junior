d69c36badbad3f20a6e0ae24c3de0bdb40bdd6da
r.boldyrev <r.boldyrev@bars.group>
2024-06-13 11:25:54 +0700

    data_structure

--- block_1/data_structure/task_1/implementation.py
+++ block_1/data_structure/task_1/implementation.py
@@ -4,6 +4,9 @@ class Tuple:
     При создании принимается последовательность объектов.
     """
 
+    def __init__(self, *args):
+        self.tuple = tuple(args)
+
     def count(self, value) -> int:
         """
         Возвращает количество появлений value в объекте.
@@ -11,7 +14,8 @@ class Tuple:
         Args:
             value: количество вхождений в объекте
         """
-        raise NotImplementedError
+        item_list = list(self.tuple)
+        return item_list.count(value)
 
     def index(self, value) -> int:
         """
@@ -20,4 +24,20 @@ class Tuple:
         Args:
             value: индекс искомого элемента
         """
-        raise NotImplementedError
+        item_list = list(self.tuple)
+        result = None
+        for idx, item in enumerate(item_list):
+            if value == item:
+                result = idx
+                if result is not None:
+                    return result
+            else:
+                continue
+
+        if result is None:
+            raise ValueError
+        else:
+            return result
+
+    def __getitem__(self, item):
+        return self.tuple[item]
--- block_1/data_structure/task_3/implementation.py
+++ block_1/data_structure/task_3/implementation.py
@@ -1,5 +1,41 @@
-def copy_dict(origin_dict: dict) -> dict:
+def copy_dict(origin_dict):
     """
     Функция возвращает копию словаря.
+    Рекурсивно копирует вложенные списки, словари, кортежи, множества.
+    Проверяется тип входных данных и в зависимости от типа множества происходит цикл копирования.
+    Внутри цикла в каждой итерации проверяется тип значения, и если тип - множество,
+     запускает эту же функцию рекурсивно.
     """
-    raise NotImplementedError
+    new_dict = {}
+    if isinstance(origin_dict, dict):
+        new_dict = {}
+        for key, val in origin_dict.items():
+            if not isinstance(val, (list, dict, tuple, set)):
+                new_dict[key] = val
+            else:
+                new_dict[key] = copy_dict(val)
+    elif isinstance(origin_dict, list):
+        new_dict = []
+        for val in origin_dict:
+            if not isinstance(val, (list, dict, tuple, set)):
+                new_dict.append(val)
+            else:
+                new_dict.append(copy_dict(val))
+    elif isinstance(origin_dict, tuple):
+        new_list = []
+        for val in origin_dict:
+            if not isinstance(val, (list, dict, tuple, set)):
+                new_list.append(val)
+            else:
+                new_list.append(copy_dict(val))
+        new_dict = tuple(new_list)
+    elif isinstance(origin_dict, set):
+        new_list = []
+        for val in origin_dict:
+            if not isinstance(val, (list, dict, tuple, set)):
+                new_list.append(val)
+            else:
+                new_list.append(copy_dict(val))
+        new_dict = set(new_list)
+
+    return new_dict
