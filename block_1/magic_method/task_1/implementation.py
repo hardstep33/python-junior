773cd1a6874a66b95e76de2d01e4825ce66e3612
r.boldyrev <r.boldyrev@bars.group>
2024-06-14 18:05:16 +0700

    magic_method

--- block_1/magic_method/task_1/implementation.py
+++ block_1/magic_method/task_1/implementation.py
@@ -1,24 +1,45 @@
 class Multiplier:
-    def __init__(self):
-        pass
+    mult = 1
+
+    def __init__(self, value):
+        self.__value = value * self.mult
+
+    @staticmethod
+    def convert_to_mult(other):
+        return other.get_value() if isinstance(other, Multiplier) else other
+
+    def __add__(self, other):
+        return Multiplier(self.__value + self.convert_to_mult(other))
+
+    def __truediv__(self, other):
+        return Multiplier(self.__value / self.convert_to_mult(other))
+
+    def __mul__(self, other):
+        return Multiplier(self.__value * self.convert_to_mult(other))
+
+    def __sub__(self, other):
+        return Multiplier(self.__value - self.convert_to_mult(other))
+
+    def get_value(self):
+        return self.__value
 
 
 class Hundred(Multiplier):
     """Множитель на 100"""
-    def __init__(self):
-        pass
+    mult = 100
 
 
 class Thousand(Multiplier):
     """Множитель на 1 000"""
-    def __init__(self):
-        pass
+    mult = 1000
 
 
 class Million(Multiplier):
     """Множитель на 1 000 000"""
-    def __init__(self):
-        pass
+    mult = 1000000
 
 
-
+# hundr = Hundred(10)
+# th = Thousand(10)
+# res = th + hundr
+# print(res.get_value())
